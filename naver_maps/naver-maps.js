fd1afe433e134729d598737e635092ae6e6c7b01
sanarurung <sanaru_@naver.com>
2025-08-15 04:18:19 +0900

    Add files via upload

--- naver_maps/naver-maps.js
+++ naver_maps/naver-maps.js
@@ -6,8 +6,19 @@ var mapOptions = {
 };
 
 var map = new naver.maps.Map('maps', {
-    center: new naver.maps.LatLng(37.3595704, 127.105399),
-    zoom: 10
+    center: new naver.maps.LatLng(37.660842, 126.993322),
+    zoom: 10,
+    scaleControl: false,
+    logoControl: false,
+    mapDataControl: false,
+    zoomControl: true,
+    minZoom: 6
+});
+
+var marker = new naver.maps.Map('maps', {
+    position: map.getCenter(),
+    map: map,
+    clickable: false
 });
 
 
