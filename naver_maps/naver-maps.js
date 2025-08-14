f9734cfa5d1a9a7f43713fb0d036f78bc0e607fb
sanarurung <sanaru_@naver.com>
2025-08-15 04:01:43 +0900

    Add files via upload

---
+++ naver_maps/naver-maps.js
@@ -0,0 +1,15 @@
+var mapDiv = document.getElementById('maps');
+
+var mapOptions = {
+    center: new naver.maps.LatLng(37.3595704, 127.105399),
+    zoom: 10
+}
+
+var map = new naver.maps.Map('map', mapOptions);
+
+
+window.navermap_authFailure = function () {
+    // 인증 실패 시 처리 코드 작성
+    // ...
+    console.log('API인증 실패')
+}
